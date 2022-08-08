4fcbbf36c951f2b01840c807e2a767de94d2b1df
adevreactjs <adevreactjs@gmail.com>
2022-08-08 12:01:34 +0300

    Updates

---
+++ static/js/NotFound.ff44e94c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpizza_shop_v2=self.webpackChunkpizza_shop_v2||[]).push([[187],{4949:function(s,u,e){e.r(u);e(2791);var n=e(2239),p=e(184);u.default=function(){return(0,p.jsx)(n.vr,{})}}}]);
+//# sourceMappingURL=NotFound.ff44e94c.chunk.js.map
